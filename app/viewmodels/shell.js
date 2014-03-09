3e4970601c7fb9fad060a19b8db844181e124ed0
Brent Pabst <me@brentpabst.com>
2014-03-09 15:33:03 -0400

    Routing

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -6,7 +6,8 @@
             router.map([
                 { route: '500', title: 'Server Error', moduleId: 'viewmodels/500', nav: false },
                 { route: '403', title: 'Access Denied', moduleId: 'viewmodels/403', nav: false },
-                { route: '', title: 'Welcome', moduleId: 'viewmodels/home', nav: true }
+                { route: '', title: 'Welcome', moduleId: 'viewmodels/home', nav: true },
+                { route: 'our-story', title: 'Our Story', moduleId: 'viewmodels/about', nav: true }
             ]).buildNavigationModel()
               .mapUnknownRoutes('viewmodels/404', '404')
               .activate();
