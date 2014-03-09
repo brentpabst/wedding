753555424842b2697a2c6139a1e7071d94bdf7fd
Brent Pabst <me@brentpabst.com>
2014-03-09 17:34:44 -0400

    Added contact and page titles

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -7,7 +7,8 @@
                 { route: '500', title: 'Server Error', moduleId: 'viewmodels/500', nav: false },
                 { route: '403', title: 'Access Denied', moduleId: 'viewmodels/403', nav: false },
                 { route: '', title: 'Welcome', moduleId: 'viewmodels/home', nav: true },
-                { route: 'our-story', title: 'Our Story', moduleId: 'viewmodels/about', nav: true }
+                { route: 'our-story', title: 'Our Story', moduleId: 'viewmodels/about', nav: true },
+                { route: 'contact', title: 'Contact', moduleId: 'viewmodels/contact', nav: true }
             ]).buildNavigationModel()
               .mapUnknownRoutes('viewmodels/404', '404')
               .activate();
